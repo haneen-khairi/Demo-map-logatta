aac6d784cecc2a9f2eb351f187cf0a0ae8be9453
John1316 <johnmaher179@gmail.com>
2023-12-08 22:41:59 +0200

    add last edits on nextjs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Layout from '@/UI/Shared/Layout'
 import MapComponent from '@/components/MapComponent'
 import SidebarInfo from '@/components/SidebarInfo'
 import { useForm } from 'react-hook-form'
+import dynamic from 'next/dynamic';
+
 
 
 export default function Home() {
@@ -19,13 +21,16 @@ export default function Home() {
   // function filterPieces(data: any){
   //   console.log("=== filterPieces ===", data)
   // }
+  function onCallBackTheMapData(data: any){
+    console.log("=== data ===")
+  }
   return <Layout>
     <div className="map-box">
       <div className="grid lg:grid-cols-3">
         <div className="col-span-2">
           {/* <form onSubmit={handleSubmit(filterPieces)}>
           </form> */}
-          <MapComponent  />
+          <MapComponent onCallBackTheMap={(e: any) => onCallBackTheMapData(e)}  />
         </div>
         <div className="col-span-1">
           <SidebarInfo />
